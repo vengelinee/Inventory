b7617f3ff57cde22436b42464ff03deee8d9a1bf
Vengelinee <vengelineangot@gmail.com>
2023-08-26 01:29:20 +0800

    Function

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,10 @@ import './App.css'
 
 const PRODUCTS = [
   { category: "Fruits", price: "$3", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: true, name: "Passionfruit" },
+  { category: "Fruits", price: "$1", stocked: false, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
   { category: "Fruits", price: "$2", stocked: true, name: "Pineapple" },
   { category: "Fruits", price: "$2", stocked: true, name: "Lemon" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Avocado" },
   { category: "Fruits", price: "$4", stocked: true, name: "Kiwi" },
   { category: "Fruits", price: "$2", stocked: false, name: "Avocado" },
   { category: "Fruits", price: "$2", stocked: true, name: "Plum" },
@@ -27,10 +26,10 @@ const PRODUCTS = [
 function ProductCategoryRow({ category }) {
   return (
     <tr>
-      <th colSpan="2">
-        {category}
-      </th>
-    </tr>
+      <th colSpan="2"> 
+        {category} 
+      </th> 
+    </tr> /* fruits and vegetable */
   );
 }
 
@@ -41,18 +40,26 @@ function ProductRow({ product }) {
     </span>;
 
   return (
-    <tr>
+    <tr class="text-left">
       <td>{name}</td>
       <td>{product.price}</td>
     </tr>
   );
 }
 
-function ProductTable({ products }) {
+function ProductTable({ products, filterText, inStockOnly }) {
   const rows = [];
   let lastCategory = null;
 
   products.forEach((product) => {
+    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+      return;
+    }
+
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+
     if (product.category !== lastCategory) {
       rows.push(
         <ProductCategoryRow
@@ -72,8 +79,8 @@ function ProductTable({ products }) {
     <table>
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Price</th>
+          <th class="text-left">Name</th>
+          <th class="text-left">Price</th>
         </tr>
       </thead>
       <tbody>{rows}</tbody>
@@ -81,22 +88,45 @@ function ProductTable({ products }) {
   );
 }
 
-function SearchBar() {
+function SearchBar({ filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange }) {
   return (
-    <form>
-      <input type="text" class="border border-gray-30 text-m rounded-xl pl-5 p-2.5 w-full mb-3" placeholder="Search ..."/> 
+    <form class="mb-3">
+      <input 
+        type="text" 
+        class="border border-gray-30 text-m rounded-xl pl-5 p-2.5 w-full mb-3" 
+        value={filterText} 
+        placeholder="Search ..."
+        onChange={(e) => onFilterTextChange(e.target.value)} /> 
         
-      <input id="bordered-checkbox-1" type="checkbox" value="" name="bordered-checkbox" class="w-4 h-4"/>
-      <label for="bordered-checkbox-1" class="w-full py-4 ml-2 text-m font-regular">Only show products in stock</label>
+      <label>
+        <input 
+          type="checkbox" 
+          checked={inStockOnly}
+          onChange={(e) => onInStockOnlyChange(e.target.checked)}
+          name="bordered-checkbox" 
+          class="w-4 h-4" />
+        {' '}
+        Only show products in stock
+      </label>
     </form>
   );
 }
 
 function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
   return (
     <div class="flex w-full flex-col rounded-3xl min-w-450px max-w-lg border-2 border-sky-600 p-10">
-      <SearchBar />
-      <ProductTable products={products} />
+      <SearchBar 
+        filterText={filterText} 
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly} />
+      <ProductTable 
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly} />
     </div>
   );
 }
